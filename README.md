625587d3a410a98ee28bc325225332d1dc0fddc7
Peter Chinetti <peter@chinetti.me>
2013-08-10 10:57:27 -0400

    Added command list in the Readme.md

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 itr-quartermaster
 =================
 
+
 A fair system to distribute money 
+
+
+
+Commands (all sent in the subject line of an email to a mailing list).
+PLEDGE $DOLLARS.CENTS GROUP_NAME
+If GROUP_NAME does not exist, it will be created.
+
+UNPLEDGE $DOLLARS.CENTS GROUP_NAME
+Return your money you pledged
+
+BUY TAG $DOLLARS.CENTS
+If the first email sent, include an order form in the body of the email.
+Other members of your group can review the order form, and if they agree with 
+the purchase, they can respond with an email with the same header.
+Once the required amount agree, the first email is forwarded to the purchasing
+agent.
--- emailgrabber.py
+++ emailgrabber.py
@@ -1,6 +1,8 @@
 import redis
 import imaplib
 import sys
+import email
+import japan_parse
 
 credfile = open('credfile','r')
 USER,PASS,IMAP_SERVER,PORT_STRING = [x.strip() for x in credfile.readlines()]
@@ -14,9 +16,15 @@ redis = redis.Redis()
 
 typ, data = recv.search(None, 'ALL')
 data_set = set(int(x) for x in data[0].split())
-redis_set = redis.smembers('read')
+redis_set = set(int(x) for x in redis.smembers('read'))
 toget = data_set - redis_set
-for target in toget:
-        typ, data = recv.fetch(target, '(RFC822)')
-        redis.set('mail:'+str(target),data)
-        redis.sadd('read',target)
+for target in data_set:
+    typ, data = recv.fetch(target, '(RFC822)')
+    mail = data[0][1]
+    message = japan_parse.parse(mail)
+    print message
+#    print type(data), len(data)
+#    print type(data[0]), data[0][1]
+#    redis.set('mail:'+str(target),data[0][1])
+#    redis.sadd('read',target)
+
